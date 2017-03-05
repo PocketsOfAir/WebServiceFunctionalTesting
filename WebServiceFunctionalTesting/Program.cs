6f88f88effd07594e565c60b377344d6442f6be6
PocketsOfAir <tcox2603@gmail.com>
2017-03-06 05:48:16 +1100

    Added Test Files

---
+++ WebServiceFunctionalTesting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WebServiceFunctionalTesting
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WebRequest request = WebRequest.Create("http://ninewebservice.apphb.com/");
+			request.Credentials = CredentialCache.DefaultCredentials;
+			request.Method = "POST";
+			//string requestData = File.ReadAllText(@"TestData.json");
+			string requestData = "nonsense";
+			byte[] requestBytes = Encoding.UTF8.GetBytes(requestData);
+			request.ContentLength = requestBytes.Length;
+			request.GetRequestStream().Write(requestBytes, 0, requestBytes.Length);
+			request.GetRequestStream().Close();
+			WebResponse response;
+			try
+			{
+				response = request.GetResponse();
+				Console.WriteLine(((HttpWebResponse)response).StatusCode);
+				byte[] responseText = new byte[response.ContentLength];
+				Stream dataStream = response.GetResponseStream();
+				dataStream.Read(responseText, 0, (int)response.ContentLength);
+				File.WriteAllBytes(@"Output.json", responseText);
+				response.Close();
+				Console.ReadKey();
+			}
+			catch (WebException exception)
+			{
+				response = exception.Response;
+				Console.WriteLine(((HttpWebResponse)response).StatusCode);
+				byte[] responseText = new byte[response.ContentLength];
+				Stream dataStream = response.GetResponseStream();
+				dataStream.Read(responseText, 0, (int)response.ContentLength);
+				File.WriteAllBytes(@"Output.json", responseText);
+				response.Close();
+				Console.ReadKey();
+			}
+		}
+	}
+}
